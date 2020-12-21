e12821f2573dd5ad4b2adeb2a70dda19214b58b1
Urvashi Chaudhary <urvashi.chaudhary@urvashic.successivetech.com>
2020-12-21 17:10:23 +0530

    React- Basic Route Setup and Layouts

---
+++ training/src/layouts/PrivateLayout/PrivateLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import { NavBar } from '../components/index';
+
+// eslint-disable-next-line no-unused-vars
+const PrivateLayout = ({ children, ...rest }) => (
+  <div>
+    <br />
+    <div>{children}</div>
+    &nbsp;
+  </div>
+);
+PrivateLayout.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  children: PropTypes.object.isRequired,
+};
+
+export default PrivateLayout;

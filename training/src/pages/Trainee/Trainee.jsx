b3e39b9dae21ef8862c6c1baed84da222dd60dcb
Urvashi Chaudhary <urvashi.chaudhary@urvashic.successivetech.com>
2021-01-04 16:47:43 +0530

    Adding Sort and onClick event on Table

--- training/src/pages/Trainee/Trainee.jsx
+++ training/src/pages/Trainee/Trainee.jsx
@@ -1,92 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { Button, withStyles } from '@material-ui/core';
-import { AddDialog } from './components/index';
-import { TableComponent } from '../../components';
-import trainees from './Data/trainee';
+import { Switch, Route } from 'react-router-dom';
+import TraineeList from './TraineeList';
+import TraineeDetail from './TraineeDetail';
 
-const useStyles = (theme) => ({
-  root: {
-    margin: theme.spacing(2),
-  },
-  dialog: {
-    textAlign: 'right',
-  },
-});
-
-class TraineeList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false,
-    };
-  }
-
-  handleClickOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleClose = () => {
-    const { open } = this.state;
-    this.setState({ open: false });
-    return open;
-  };
-
-  handleSubmit = (data) => {
-    this.setState({
-      open: false,
-    }, () => {
-      // eslint-disable-next-line no-console
-      console.log(data);
-    });
-  }
-
-  render() {
-    const { open } = this.state;
-    const { match: { url }, classes } = this.props;
-    return (
-      <>
-        <div className={classes.root}>
-          <div className={classes.dialog}>
-            <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
-              ADD TRAINEELIST
-            </Button>
-            <AddDialog open={open} onClose={this.handleClose} onSubmit={() => this.handleSubmit} />
-          </div>
-          <TableComponent
-            id="id"
-            data={trainees}
-            column={
-              [
-                {
-                  field: 'name',
-                  label: 'Name',
-                  align: 'center',
-                },
-                {
-                  field: 'email',
-                  label: 'Email Address',
-                },
-              ]
-            }
-          />
-          <ul>
-            {trainees.map(({ name, id }) => (
-              <li key={id}>
-                <Link to={`${url}/${id}`}>
-                  {name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </>
-    );
-  }
+function Trainee(props) {
+  const { match: { path } } = props;
+  return (
+    <Switch>
+      <Route exact path={path} component={TraineeList} />
+      <Route exact path={`${path}/:traineeId`} component={TraineeDetail} />
+    </Switch>
+  );
 }
-TraineeList.propTypes = {
+Trainee.propTypes = {
   match: PropTypes.objectOf(PropTypes.object).isRequired,
-  classes: PropTypes.objectOf(PropTypes.string).isRequired,
 };
-export default withStyles(useStyles)(TraineeList);
+export default Trainee;

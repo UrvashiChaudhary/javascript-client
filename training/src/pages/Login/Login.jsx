184fa97a82c47ae8fd0b2128acc980a1e66f7e51
Urvashi Chaudhary <urvashi.chaudhary@urvashic.successivetech.com>
2021-01-07 12:20:55 +0530

    Spinner, Axios login and routes authorize

--- training/src/pages/Login/Login.jsx
+++ training/src/pages/Login/Login.jsx
@@ -5,11 +5,9 @@ import {
   TextField, Typography, CardContent, InputAdornment, Button, Avatar, Card, CssBaseline, withStyles,
   CircularProgress,
 } from '@material-ui/core';
-import { LockOutlined } from '@material-ui/icons';
-import localStorage from 'local-storage';
 import { Redirect } from 'react-router-dom';
-import { schema } from '../../config/constants'
-import { Email, VisibilityOff } from '@material-ui/icons';
+import { Email, VisibilityOff, LockOutlined } from '@material-ui/icons';
+import { schema } from '../../config/constants';
 import callApi from '../../libs/utils/api';
 import { snackbarContext } from '../../contexts/index';
 
@@ -24,10 +22,10 @@ const Design = (theme) => ({
     marginTop: theme.spacing(25),
     marginLeft: theme.spacing(50),
   },
-})
+});
 
 class Login extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       loader: false,
@@ -41,6 +39,13 @@ class Login extends React.Component {
       },
     };
   }
+
+  renderRedirect = () => {
+    const { redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/trainee" />;
+    }
+  };
 
   handleChange = (key) => ({ target: { value } }) => {
     this.setState({ [key]: value });
@@ -83,35 +88,34 @@ class Login extends React.Component {
         disabled: true,
         loader: true,
       });
-      await callApi('post', '/user/login/', { email, password }, value);
-      console.log('call api', email, password);
-      this.setState({
-        disabled: false,
-        loader: false,
-      });
-      console.log('after call api');
-      if (localStorage.get('token')) {
-        console.log('in if call api');
+      const resp = await callApi('post', '/user/login/', { email: email.trim(), password });
+      console.log('resp', resp);
+      if (resp.data.data && (resp.data.status === 200)) {
+        window.localStorage.setItem('token', resp.data.data);
         this.setState({
           redirect: true,
+          message: 'Successfully Login',
+        }, () => {
+          const { message } = this.state;
+          value(message, 'success');
+        });
+      } else {
+        this.setState({
+          message: 'Email not Registered',
+        }, () => {
+          const { message } = this.state;
+          value(message, 'error');
         });
       }
     };
 
-    renderRedirect = () => {
-      const { redirect } = this.state;
-      if (redirect) {
-        return <Redirect to="/trainee" />;
-      }
-    };
+    render() {
+      const { classes } = this.props;
+      const { loader } = this.state;
 
-  render() {
-    const { classes } = this.props;
-    const { loader } = this.state;
-
-    return (
-      <>
-         <div className={classes.main}>
+      return (
+        <>
+          <div className={classes.main}>
             <CssBaseline />
             <Card open aria-labelledby="form-dialog-title">
               <Avatar className={classes.icon}>
@@ -141,8 +145,7 @@ class Login extends React.Component {
                       }}
                     />
                   </div>
-              <br />
-
+                  <br />
                   <div>
                     <TextField
                       type="password"
@@ -166,15 +169,15 @@ class Login extends React.Component {
                   </div>
               &nbsp;
                   <div>
-                  <snackbarContext.Consumer>
-                    {(value) => (
-                      <Button variant="contained" color="primary" onClick={() => this.onClickHandler(value)} disabled={this.hasErrors()} fullWidth>
-                        {this.renderRedirect()}
-                        <span>{loader ? <CircularProgress size={20} /> : ''}</span>
-                        SIGN IN
-                      </Button>
-                    )}
-                  </snackbarContext.Consumer>
+                    <snackbarContext.Consumer>
+                      {(value) => (
+                        <Button variant="contained" color="primary" onClick={() => this.onClickHandler(value)} disabled={this.hasErrors()} fullWidth>
+                          {this.renderRedirect()}
+                          <span>{loader ? <CircularProgress size={20} /> : ''}</span>
+                          SIGN IN
+                        </Button>
+                      )}
+                    </snackbarContext.Consumer>
                   </div>
                 </form>
               </CardContent>
